7caaaf76c05b47085e4667ec7e23b1fca3602023
santiagoxx81 <santiagoxx81@hotmail.com>
2025-08-25 12:49:02 -0300

    criado o service e cadastro ts

---
+++ front/src/app/cadastro/cadastro.component.ts
@@ -0,0 +1,62 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { PoliciaisService, Policial } from '../services/policiais.service';
+
+
+@Component({
+  selector: 'app-cadastro',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './cadastro.component.html',
+  styleUrl: './cadastro.component.css'
+})
+export class CadastroComponent implements OnInit {
+
+  policiais: Policial[] = [];
+  policial: Policial = {
+    rg_civil: '',
+    rg_militar: '',
+    cpf: '',
+    data_nascimento: '',
+    matricula: ''
+  };
+  editando = false;
+  idEdicao = 0;
+
+  constructor(private service: PoliciaisService) {}
+
+  ngOnInit(): void {
+    this.listarPoliciais();
+  }
+
+  listarPoliciais(): void {
+    this.service.listarPoliciais().subscribe(data => this.policiais = data);
+  }
+
+  salvar(): void {
+    if (this.editando) {
+      this.service.atualizarPolicial(this.idEdicao, this.policial).subscribe(() => {
+        this.editando = false;
+        this.policial = { rg_civil: '', rg_militar: '', cpf: '', data_nascimento: '', matricula: '' };
+        this.listarPoliciais();
+      });
+    } else {
+      this.service.cadastrarPolicial(this.policial).subscribe(() => {
+        this.policial = { rg_civil: '', rg_militar: '', cpf: '', data_nascimento: '', matricula: '' };
+        this.listarPoliciais();
+      });
+    }
+  }
+
+  editar(p: Policial): void {
+    this.policial = { ...p };
+    this.idEdicao = p.id!;
+    this.editando = true;
+  }
+
+  excluir(id: number): void {
+    this.service.deletarPolicial(id).subscribe(() => this.listarPoliciais());
+  }
+
+}
---
+++ front/src/app/services/policiais.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Policial {
+    id?: number;
+    rg_civil: string;
+    rg_militar: string;
+    cpf: string;
+    data_nascimento: string;
+    matricula: string;
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PoliciaisService {
+    private api = 'http://localhost:3012/policiais';
+
+    constructor(private http: HttpClient) { }
+
+    listarPoliciais(cpf?: string, rg?: string): Observable<Policial[]> {
+        let params = new HttpParams();
+        if (cpf) {
+            params = params.set('cpf_input', cpf);
+        } else if (rg) {
+            params = params.set('rg_input', rg);
+        }
+        return this.http.get<Policial[]>(this.api, { params });
+    }
+
+    cadastrarPolicial(policial: Policial): Observable<any> {
+        return this.http.post(this.api, policial);
+    }
+
+    atualizarPolicial(id: number, policial: Policial): Observable<any> {
+        return this.http.put(`${this.api}/${id}`, policial);
+    }
+
+    deletarPolicial(id: number): Observable<any> {
+        return this.http.delete(`${this.api}/${id}`);
+    }
+}
